71a422952d1ac96ea5168d44a2d21a8ba1cc8f61
J Litt <jdlitton2594@gmail.com>
2022-05-25 22:55:53 -0500

    changing html and css plus initial js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,43 @@
 // Assignment code here
+var generatePassword = function () {
+  const length = lengthEl.value;
+  const hasLower = lowerEl.checked;
+  const hasUpper = upperEl.checked;
+  const hasNumbers = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
 
+  console.log(length);
+}
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length')
+const upperEl = document.getElementById('upper');
+const lowerEl = document.getElementById('lower');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
 
+const passwordFunc = {
+  lower: getLower,
+  upper: getUpper,
+  number: getNumber,
+  symbol: getSymbol
+};
+
+function getLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getNumber () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getSymbol () {
+  const symbols = '!@#$%^&*(){}[]+<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
