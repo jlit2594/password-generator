7676f11fab42021bfc871882a0c454d93f9cfb96
J Litt <jdlitton2594@gmail.com>
2022-05-26 23:50:19 -0500

    I tried

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,64 @@
 // Assignment code here
-var generatePassword = function () {
-  const length = lengthEl.value;
-  const hasLower = lowerEl.checked;
-  const hasUpper = upperEl.checked;
-  const hasNumbers = numbersEl.checked;
-  const hasSymbols = symbolsEl.checked;
-
-  console.log(length);
-}
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length')
-const upperEl = document.getElementById('upper');
-const lowerEl = document.getElementById('lower');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-
-const passwordFunc = {
-  lower: getLower,
-  upper: getUpper,
-  number: getNumber,
-  symbol: getSymbol
-};
-
-function getLower () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getUpper () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getNumber () {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getSymbol () {
-  const symbols = '!@#$%^&*(){}[]+<>/,.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-// Get references to the #generate element
+var lengthEl = document.getElementById('length')
+var upperEl = document.getElementById('upper');
+var lowerEl = document.getElementById('lower');
+var numbersEl = document.getElementById('numbers');
+var symbolsEl = document.getElementById('symbols');
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var length = +lengthEl.value;
+var hasLower = lowerEl.checked;
+var hasUpper = upperEl.checked;
+var hasNumbers = numbersEl.checked;
+var hasSymbols = symbolsEl.checked;
 
-  passwordText.value = password;
+function generatePassword(length, upper, lower, numbers, symbols) {
+  var generatedPassword = '';
 
+  var typesCount = lower + upper + numbers + symbols;
+
+  var typesArr = ['lower', 'upper', 'numbers', 'symbols'].filter(item => Object.values(item)[0]);
+
+  if(typesCount === 0) {
+    return '';
+  }
+
+  for(let i=0; i<length; i++) {
+		typesArr.forEach(type => {
+			generatedPassword += [getLower(), getUpper(), getNumber(), getSymbol()];
+		});
+	}
+
+  var finalResult = generatedPassword.slice(0, length);
+  return finalResult;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getLower () {
+  if (lowerEl.checked) {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}}
+
+function getUpper () {
+  if (upperEl.checked) {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}}
+
+function getNumber () {
+  if (numbersEl.checked) {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}}
+
+function getSymbol () {
+  if (symbolsEl.checked) {
+  var symbols = '!@#$%^&*(){}[]+<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}}
+
+function writePassword () {
+  var password = generatePassword(length, hasUpper, hasLower, hasNumbers, hasSymbols);
+  var passwordText = document.querySelector("#password")
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword)
